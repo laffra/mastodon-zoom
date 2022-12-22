dd0537b837a9b2aebef67047f3b3a55febc1ed24
laffra <laffra@gmail.com>
2022-12-22 10:56:39 +0100

    Add more hosts

--- contentscript.js
+++ contentscript.js
@@ -2,71 +2,68 @@
     let index = 0;
     let enabled = false;
 
-    function focus() {
+    function zoom() {
         $('article').css({
-            visibility: "hidden",
-            position: "absolute",
+            visibility: enabled ? "hidden" : "visible",
+            position: enabled ? "absolute" : "static",
         });
         $('article').eq(index).css({
             visibility: "visible",
             position: "static",
         });
+        $(".tabs-bar__wrapper,button.load-more,.columns-area__panels__pane__inner").css({
+            visibility: enabled ? "hidden" : "visible",
+        })
+        $('.scrollable').css({
+            backgroundColor: "transparent",
+        });
         if (index > $('article').length - 5) {
             $("button.load-more").click();
             console.log("load more", index, $('article').length)
         }
-        $(".tabs-bar__wrapper,button.load-more,.columns-area__panels__pane__inner").css({
-            visibility: enabled ? "hidden" : "visible",
-        })
-        if (!enabled) {
-            $('article').eq(index).css({
-                visibility: "visible",
-                position: "static",
-            });
-        } else {
-            $('.scrollable').css({
-                backgroundColor: "transparent",
-            });
-        }
     }
 
     $(document.body).on("keydown", (event) => {
+        console.log("Mastodon-zoom:", event.key, event.target.tagName);
         if (event.target.tagName != "DIV") return;
         switch (event.key) {
-        case 'ArrowDown':
         case 'j':
+        case 'ArrowDown':
             index = Math.min(index + 1, $('article').length - 1);
             break;
-        case 'ArrowUp':
         case 'k':
+        case 'ArrowUp':
             index = Math.max(index - 1, 0);
-            break;
-        case 'Home':
-            index = 0;
-            break;
-        case 'End':
-            index = $('article').length - 1;
             break;
         case 'z':
             enabled = !enabled;
+            if (enabled) event.target.requestFullscreen();
             break;
         case '?':
             setTimeout(() => {
+                console.log("Mastodon Zoom: Add shortcuts")
                 $(".keyboard-shortcuts").find("tbody").prepend(
-                    $("<tr><td><kbd>z</kbd></td><td><span>Toggle zoom mode (Mastodon Zoom extension)</span></td></tr>")
+                    $("<tr><td><kbd>z</kbd></td><td><span>Toggle zoom mode (Mastodon Zoom extension)</span></td></tr>"),
+                    $("<tr><td><kbd>f</kbd></td><td><span>Enter full-screen mode (Mastodon Zoom extension)</span></td></tr>"),
                 );
             }, 1);
             break;
         default:
             console.log("Mastodon-zoom:", event.key);
         }
-        focus();
+        zoom();
     });
             
     setInterval(() => {
         if (enabled) {
-            focus();
+            zoom();
         }
     }, 500)
-    console.log("Mastodon Zoom extension was loaded. Type 'Z' to toggle.")
+
+    console.log('Mastodon Zoom: Extension was loaded. Usage:');
+    console.log(' - Press "Esc" to unfocus textarea/search.');
+    console.log(' - Press "z" to toggle the zoom.');
+    console.log(' - Press "j" or "down" to go to the next article.');
+    console.log(' - Press "k" or "up" to go to the previous article.');
+    console.log('See https://github.com/laffra/mastodon-zoom.');
 })();
